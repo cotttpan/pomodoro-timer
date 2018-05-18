ddd33d807c8023c671596bf66986c1acec622f1c
cotttpan <cotttpan@gmail.com>
2018-05-18 22:56:27 +0900

    feat(service/app-session)

--- src/service/app-session.ts
+++ src/service/app-session.ts
@@ -1,62 +1,30 @@
 import { merge } from 'rxjs'
 import { filter, map } from 'rxjs/operators'
-import { EventSource, select, scoped } from 'command-bus'
+import { EventSource, select } from 'command-bus'
 import { existy } from '@cotto/utils.ts'
-import { Storage } from '@/lib/storage'
-import { Todo } from '@/model/todos'
-import { RepositoryGroup, INTENTS, TODOS } from './common'
+import { RepositoryGroup, INTENTS, APP_SESSION, TODO } from './shared'
+import { appSessionService } from '@/domain/app-session'
 
-//
-// ─── TYPES ──────────────────────────────────────────────────────────────────────
-//
-export interface AppSession {
-  currentTimerTarget?: Todo
-}
-
-export type AppSessionRepository = Storage<AppSession>
-
-//
-// ─── REPOSITORY ─────────────────────────────────────────────────────────────────
-//
-export const createAppSessionRepository = (): AppSessionRepository => {
-  const init = () => ({ currentTimerTarget: undefined })
-  return new Storage<AppSession>(init)
-}
-
-//
-// ─── COMMAND ────────────────────────────────────────────────────────────────────
-//
-const RESULT = scoped('APP_SESSION/RESULT/')
-export const RESULTS = {
-  CHANGE: RESULT<AppSession>('CHANGE'),
-}
-
-//
-// ─── SERVICE ────────────────────────────────────────────────────────────────────
-//
 export const currentTimerTargetEpic = (ev: EventSource, repo: RepositoryGroup) => {
   return merge(
     select(ev, INTENTS.POMODORO_TIMER_START).pipe(
       map(action => String(action.payload.todoId)),
-      map(id => repo.todos.get(id)),
+      map(id => repo.todo.get(id)),
       filter(existy),
-      map(todo => repo.appSession.set({ currentTimerTarget: todo })),
-      map(RESULTS.CHANGE),
+      map(todo => APP_SESSION.INPUT.PATCH(s => ({ ...s, currentTimerTarget: todo }))),
     ),
     select(ev, INTENTS.POMODORO_TIMER_RESET).pipe(
-      map(() => repo.appSession.set({ currentTimerTarget: undefined })),
-      map(RESULTS.CHANGE),
+      map(() => APP_SESSION.INPUT.PATCH(s => ({ ...s, currentTimerTarget: undefined }))),
     ),
-    select(ev, TODOS.CHANGE).pipe(
+    select(ev, TODO.OUTPUT.CHANGE).pipe(
       map(() => repo.appSession.get('currentTimerTarget')),
-      map(todo => todo ? String(todo.id) : ''),
-      map(id => repo.todos.get(id)),
-      map(todo => repo.appSession.set({ currentTimerTarget: todo })),
-      map(RESULTS.CHANGE),
+      map(target => target ? String(target.id) : ''),
+      map(id => repo.todo.get(id)),
+      map(todo => APP_SESSION.INPUT.PATCH(s => ({ ...s, currentTimerTarget: todo }))),
     ),
   )
 }
 
 export const bootAppSessionService = (ev: EventSource, repo: RepositoryGroup) => {
-  return currentTimerTargetEpic(ev, repo)
+  return appSessionService(currentTimerTargetEpic(ev, repo), { repo: repo.appSession })
 }
