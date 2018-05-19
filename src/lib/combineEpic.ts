63369f98fadb0fa86203e1a611609dbbd7b2f62d
cotttpan <cotttpan@gmail.com>
2018-05-19 10:20:27 +0900

    feat(lib/combineEpic): api arguments in proxy function

--- src/lib/combineEpic.ts
+++ src/lib/combineEpic.ts
@@ -5,14 +5,14 @@ export interface Epic<T> {
   (ev: EventSource, api: T): Observable<Command | null>
 }
 
-export interface Proxy {
-  (src$: Observable<Command | null>): Observable<Command | null>
+export interface Proxy<T = any> {
+  (src$: Observable<Command | null>, api: T): Observable<Command | null>
 }
 
-export default function combineEpic<T>(epics: Epic<T>[], proxy?: Proxy) {
+export default function combineEpic<T>(epics: Epic<T>[], proxy?: Proxy<T>) {
   return (ev: EventSource, api: T) => {
     const observables = epics.map(f => f(ev, api))
     const command$ = merge(...observables)
-    return proxy ? command$.pipe(proxy) : command$
+    return proxy ? command$.pipe($ => proxy($, api)) : command$
   }
 }
